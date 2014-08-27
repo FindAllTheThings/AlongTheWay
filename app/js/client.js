29782d0058a82e9141b676340ae9da2e42bc9e02
lwscroggins <larry.scroggins@gmail.com>
2014-08-27 16:08:41 -0700

    fixed map rendering and formview in backbone

--- app/js/client.js
+++ app/js/client.js
@@ -1,22 +1,24 @@
 var $ = require('jquery');
 var Map = require('./maps/models/map-model');
 var MapView = require('./maps/views/map-view');
+var FormView = require('./maps/views/form-view');
 
 var map = new Map();
 var mapView = new MapView({model: map});
+var formView = new FormView();
 
 $(function(){
 
-    $('#map-wrapper').html(mapView.el);
+    $('#map-canvas').html(mapView.el);
 
-    // bind handle
-    $('#overlay-handle').on('click',function(){
-      $('#options-wrapper').toggleClass('open');
-    });
+    // // bind handle
+    // $('#overlay-handle').on('click',function(){
+    //   $('#options-wrapper').toggleClass('open');
+    // });
 
-    // close overlay on execute
-    $('#go').on('click', function(){
-      $('#options-wrapper').toggleClass('open');
-    });
+    // // close overlay on execute
+    // $('#go').on('click', function(){
+    //   $('#options-wrapper').toggleClass('open');
+    // });
 
 });
